871c001867f6705ae719441337a420eb428e121c
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-25 15:43:13 +0100

    altered tests and docking station with new bikes array

--- feature_test.rb
+++ feature_test.rb
@@ -4,3 +4,10 @@ require './lib/docking_station.rb'
 station = DockingStation.new
 bike = Bike.new
 
+
+
+p station.dock(Bike.new)
+p station.dock(Bike.new)
+p station.dock(Bike.new)
+
+
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,19 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    raise 'No bikes in station!' unless @bikes > 0
-    @bikes -= 1
+    raise 'No bikes in station!' unless @bikes.size > 0
+    @bikes.shift
     Bike.new
   end
 
   def dock(bike)
-     raise "Dock full!" unless bike < 2
-     @bikes = bike
+     raise "Dock full!" unless @bikes.size < 20
+     @bikes << Bike.new
   end
 
 end
