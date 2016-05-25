41d9c5cd9cbd56448dae605ce9b6d2462406aef3
Anna <anna.hollandsmith3@gmail.com>
2016-05-25 14:00:46 +0100

    initial commit day_two

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
+	
 	def release_bike
 		Bike.new
+	end
+
+	def dock(bike)
+		@bike
+	end
+
+	def bike
+		@bike
+
 	end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,12 +2,30 @@
 
 require 'DockingStation'
 
+
 describe DockingStation do
-	it "responds to release_bike" do
+	#it {is_expected.to respond_to :release_bike}
+
+	it 'responds to release_bike' do
 		expect(subject).to respond_to :release_bike
 	end
+
 	it "releases the working bike" do
 		bike = subject.release_bike
 		expect(bike).to be_working
-		end 
+	
+	end
+
+	#it {is expected_to respond_to(:dock).with(1).argument}
+	it 'docks a bike' do
+		#expect(subject).to respond_to(:dock).with(1).argument
+		expect(subject.dock(bike)).to eq bike
+	end
+
+	it 'responds to bike' do 
+		expect(subject).to respond_to :bike
+	end
+	  
+
+	
 end
