c2b653cf5534027ac75588317e3dc05abeb28a94
awye765 <awye765@googlemail.com>
2016-05-26 12:40:33 +0100

    Refactored to create single responsibility for full and empty methods

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -4,18 +4,30 @@ class DockingStation
 
   attr_reader :bike
 
-  def initialize 
+  def initialize
     @bikes = []
   end
 
   def dock_bike(bike)
-  	fail 'Error: Docking Station Full' if @bikes.count >= 20
+  	fail 'Error: Docking Station Full' if full?
 	  @bikes << bike
   end
 
   def release_bike
-    fail 'Error. No bikes available' if @bikes.empty?
+    fail 'Error. No bikes available' if empty?
     @bikes.pop
   end
 
+  private
+
+  def full?
+    @bikes.count >= 20
   end
+
+  def empty?
+    @bikes.empty?
+  end
+
+  end
+
+
