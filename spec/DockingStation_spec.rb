a1e6617c3469a63d3129717ba62504e95cb57077
awye765 <awye765@googlemail.com>
2016-05-27 09:21:28 +0100

    Refactoring rspec file for readability

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -15,7 +15,7 @@ describe DockingStation do
       expect(bikes).to be_working
     end
 
-    it "won't release a broken bike",:focus=>true do
+    it "won't release a broken bike" do
       bike = Bike.new
       bike.report_broken
       subject.dock_bike bike
@@ -31,42 +31,41 @@ describe DockingStation do
     end
 
   describe '#release_bike' do
-      it 'releases bike' do
+    it 'releases bike' do
         bikes = Bike.new
         subject.dock_bike(bikes)
         expect(subject.release_bike).to eq bikes
-      end
+    end
+
     it 'raises error if no bikes available' do
       expect {subject.release_bike}.to raise_error 'Error. No bikes available'
     end
+  end
 
   describe '#dock_bike' do
+
     it 'raises error when station is full' do
       subject.capacity.times { subject.dock_bike Bike.new }
       expect {subject.dock_bike(Bike.new)}.to raise_error 'Error: Docking Station Full'
+    end
   end
-  end
-
 
   describe 'initialization' do
-  it 'has a variable capacity' do
+    it 'has a variable capacity' do
     docking_station = DockingStation.new(50)
     50.times { docking_station.dock_bike Bike.new }
     expect{ docking_station.dock_bike Bike.new }.to raise_error 'Error: Docking Station Full'
-  end
-  end
-
-
-describe 'initialization' do
-  subject { DockingStation.new }
-  let(:bikes) { Bike.new }
-  it 'defaults capacity' do
-    described_class::DEFAULT_CAPACITY.times do
-      subject.dock_bike(bikes)
     end
-    expect{ subject.dock_bike(bikes) }.to raise_error 'Error: Docking Station Full'
   end
-end
 
- end
-end
+    subject { DockingStation.new }
+    let(:bikes) { Bike.new }
+
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock_bike(bikes)
+    end
+
+    expect{ subject.dock_bike(bikes) }.to raise_error 'Error: Docking Station Full'
+    end
+  end
